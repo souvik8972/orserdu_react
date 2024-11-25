af65da4771832dbbee207e9fdca65ec2d7b74f8f
Souvik Das <souvik8582@gmail.com>
2024-11-25 16:31:14 +0530

    removed some dependencies

--- src/Components/HeroContainer/HeroContainer.jsx
+++ src/Components/HeroContainer/HeroContainer.jsx
@@ -19,7 +19,7 @@ const HeroContainer = ({ children, img, navLink,navText }) => {
 
           {/* Navigation Section */}
           {navLink && (
-            <NavLink to={navLink} className=" hover:underline font-semibold text-dark_green absolute -bottom-5 bg-white shadow_round p-2 flex items-center justify-center  w-[18%] h-12 rounded-3xl ">
+            <NavLink to={navLink} className=" hover:underline  text-dark_green absolute -bottom-5 bg-white shadow_round p-2 flex items-center justify-center  w-[18%] h-12 rounded-3xl ">
               <div className="w-[85%] p-3 flex justify-between items-center h-full ">
             <div className="text-[18px]  " >
               {navText}
